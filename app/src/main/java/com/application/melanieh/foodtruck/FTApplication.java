7c41f82935fabd3ab23d0120cf92f71b67cc993b
melaniejae <mjhanddev@gmail.com>
2017-05-22 08:23:04 -0700

    Type of commit: feature
    changes made: add gradle dependencies for external libraries
    
    resolves issues: allows access to libraries for certain app features

---
+++ app/src/main/java/com/application/melanieh/foodtruck/FTApplication.java
@@ -0,0 +1,11 @@
+package com.application.melanieh.foodtruck;
+
+import android.app.Application;
+
+/**
+ * Created by melanieh on 5/22/17.
+ */
+
+public class FTApplication extends Application {
+
+}
